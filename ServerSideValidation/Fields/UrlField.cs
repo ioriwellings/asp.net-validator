acc8b045b3beb97018bf51da442850e3912df624
Onur <onur@pontdigital.com>
2013-07-28 19:42:29 +0300

    * Url Field improvements * Migrate to vs 2012

--- ServerSideValidation/Fields/UrlField.cs
+++ ServerSideValidation/Fields/UrlField.cs
@@ -4,7 +4,7 @@
     {
         public UrlField(string input) : base(input)
         {
-            Pattern = @"^(http(s)?:\/\/)?([a-zA-Z0-9\%\-\_\?\.]+\.)+[a-zA-Z0-9\%\-\?\=\/\.]+$";
+            Pattern = @"^(http(s)?:\/\/)?([a-zA-Z0-9_\%\-\?\.]+\.)+[a-zA-Z0-9_\%\-\?\=\/\.]+$";
             PatternError = PatternError = Global.ResourceManager.GetString("url", Global.CultureInfo);
         }
     }
